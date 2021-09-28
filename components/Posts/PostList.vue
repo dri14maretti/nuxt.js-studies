cdde056662e99b9377f9f935396ee83ac7d470de
Adriano Maretti <dri.cmaretti@gmail.com>
2021-09-28 09:12:14 -0300

    first commit

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,39 @@
+<template>
+	<section class="post-list">
+		<PostPreview v-bind:key="post.id" v-for="post of posts" :postInfo="post" />
+	</section>
+</template>
+
+<script>
+    import PostPreview from '~/components/Posts/PostPreview'
+
+export default {
+	components: {
+		PostPreview,
+	},
+	data: () => ({
+		posts: [
+			{
+				id: '1',
+				title: 'Hello there!',
+				previewText:
+					'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quos.',
+				thumbnail:
+					'https://blog.vulpi.com.br/wp-content/uploads/2020/04/apple-coffee-computer-desk-356056-1024x680.jpg',
+			},
+		],
+	}),
+}
+</script>
+
+<style scoped>
+.post-list {
+	display: flex;
+	padding: 20px;
+	box-sizing: border-box;
+	flex-wrap: wrap;
+	align-items: center;
+	justify-content: center;
+}
+
+</style>
---
+++ pages/admin/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="admin-page">
+        <section class="new-post">
+            <v-btn>
+                Create Post
+            </v-btn>
+        </section>
+        <section>
+            <h1>Existing posts</h1>
+            <PostList />
+        </section>
+    </div>
+</template>
+
+<script>
+import PostList from '~/components/Posts/PostList'
+
+export default defineComponent({
+    components: {
+        PostList
+    }
+})
+</script>
